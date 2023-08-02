f30fe6fe0a955515ab062e64f0c47c8f7866b4f6
DustWolverine <akashrajnitrr@gmail.com>
2023-08-02 08:43:38 +0530

    weather apiprogramm

--- weather_app.py
+++ weather_app.py
@@ -1,17 +1,16 @@
 import requests
 
-API_URL = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22"  # Replace this with the actual API URL
+API_URL = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22"  
 
 
 def get_weather_data(date):
-    # Make an API request to get the weather data for the given date
     response = requests.get(API_URL)
     if response.status_code == 200:
         weather_data = response.json()
         for item in weather_data["list"]:
             if item["dt_txt"].startswith(date):
                 return item["main"]["temp"]
-        print("Weather data not found for the specified date.")
+        print("Weather data not found.")
         return None
     else:
         print("Failed to fetch weather data from the API.")
@@ -19,14 +18,13 @@ def get_weather_data(date):
 
 
 def get_wind_speed(date):
-    # Make an API request to get the weather data for the given date
     response = requests.get(API_URL)
     if response.status_code == 200:
         weather_data = response.json()
         for item in weather_data["list"]:
             if item["dt_txt"].startswith(date):
                 return item["wind"]["speed"]
-        print("Weather data not found for the specified date.")
+        print("Weather data not found.")
         return None
     else:
         print("Failed to fetch weather data from the API.")
@@ -34,14 +32,13 @@ def get_wind_speed(date):
 
 
 def get_pressure(date):
-    # Make an API request to get the weather data for the given date
     response = requests.get(API_URL)
     if response.status_code == 200:
         weather_data = response.json()
         for item in weather_data["list"]:
             if item["dt_txt"].startswith(date):
                 return item["main"]["pressure"]
-        print("Weather data not found for the specified date.")
+        print("Weather data not found.")
         return None
     else:
         print("Failed to fetch weather data from the API.")
@@ -61,17 +58,17 @@ def main():
             date = input("Enter the date (YYYY-MM-DD): ")
             temperature = get_weather_data(date)
             if temperature is not None:
-                print(f"The temperature on {date} is {temperature} K.")
+                print(f"The temperature on {date} is {temperature}.")
         elif choice == "2":
             date = input("Enter the date (YYYY-MM-DD): ")
             wind_speed = get_wind_speed(date)
             if wind_speed is not None:
-                print(f"The wind speed on {date} is {wind_speed} m/s.")
+                print(f"The wind speed on {date} is {wind_speed}")
         elif choice == "3":
             date = input("Enter the date (YYYY-MM-DD): ")
             pressure = get_pressure(date)
             if pressure is not None:
-                print(f"The pressure on {date} is {pressure} hPa.")
+                print(f"The pressure on {date} is {pressure}")
         elif choice == "0":
             print("Exiting the program.")
             break
